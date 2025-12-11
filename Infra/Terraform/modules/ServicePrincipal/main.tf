b66488b536aa602500fbf295673ef0d57ceceaef
rahul_dondeti <rahulnani2@gmail.com>
2025-12-10 23:06:03 -0500

    Update main.tf

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -6,8 +6,13 @@ resource "azuread_application" "azapp01" {
    owners = [ data.azuread_client_config.azclconfig01.object_id]
 }
 
+resource "time_sleep" "wait_for_application" { 
+  depends_on = [azuread_application.azapp01]
+  create_duration = "60s"
+}
+
 resource "azuread_service_principal" "azadsp01" {
-   depends_on=[azuread_application.azapp01]
+   depends_on=[time_sleep.wait_for_replication]
    app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
    owners = [ data.azuread_client_config.azclconfig01.object_id ]
