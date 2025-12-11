90686991be0f81f1ea1a081bca7a3b26a3ee4996
rahul_dondeti <rahulnani2@gmail.com>
2025-12-10 23:00:07 -0500

    Add dependencies to Service Principal resources

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -7,11 +7,13 @@ resource "azuread_application" "azapp01" {
 }
 
 resource "azuread_service_principal" "azadsp01" {
+   depends_on=[azuread_application.azapp01]
    app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
    owners = [ data.azuread_client_config.azclconfig01.object_id ]
 }
 
 resource "azuread_service_principal_password" "azadsppasswd01" {
+  depends_on=[azuread_application.azapp01,azuread_service_principal.azadsp01]
   service_principal_id = azuread_service_principal.azadsp01.id
 }
