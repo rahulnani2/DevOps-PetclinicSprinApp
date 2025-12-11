2b60165be35f3faa2c9c54c98385988780be28d5
rahul_dondeti <rahulnani2@gmail.com>
2025-12-10 23:06:03 -0500

    Update main.tf

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -12,7 +12,7 @@ resource "azuread_application" "azapp01" {
 
 
 resource "azuread_service_principal" "azadsp01" {
-   depends_on=[azuread_application.azapp01]
+   depends_on=[time_sleep.wait_for_replication]
    app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
    use_existing = true
