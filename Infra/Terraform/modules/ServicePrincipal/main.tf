759d7b118339bd80e4dbfd5c3dffe071a7db0af5
rahulnani2 <rahulnani2@gmail.com>
2025-12-12 22:22:18 -0500

    Updated the ServicePrincipal main blocks to remove life cycle rules

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -2,25 +2,20 @@ data "azuread_client_config" "azclconfig01" {}
 
 resource "azuread_application" "azapp01" {
    display_name = var.service_principal_name
-   owners = [ data.azuread_client_config.azclconfig01.object_id]
+ #  owners = [ data.azuread_client_config.azclconfig01.object_id]
 }
 
-#resource "time_sleep" "wait_for_application" { 
-#  depends_on = [azuread_application.azapp01]
-#  create_duration = "60s"
-#}
 
 resource "azuread_service_principal" "azadsp01" {
- #  depends_on = [time_sleep.wait_for_application, azuread_application.azapp01]
    app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
-   owners = [ data.azuread_client_config.azclconfig01.object_id ]
- lifecycle { 
-   create_before_destroy = true 
-   }
+ #  owners = [ data.azuread_client_config.azclconfig01.object_id ]
 }  
 
 resource "azuread_service_principal_password" "azadsppasswd01" {
   depends_on=[azuread_application.azapp01, azuread_service_principal.azadsp01]
   service_principal_id = azuread_service_principal.azadsp01.id
+   rotate_when_changed = {
+     rotation = timestamp()
+   }
 }
