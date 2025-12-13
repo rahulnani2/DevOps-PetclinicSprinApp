25acb3cbbb949de7692a5629dff9fabfc1a66837
rahul_dondeti <rahulnani2@gmail.com>
2025-12-12 23:58:38 -0500

    Add use_existing attribute to service principal

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -17,6 +17,7 @@ resource "azuread_service_principal" "azadsp01" {
    depends_on = [ time_sleep.wait_for_app ]
    #app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
+   use_existing = true
  #  owners = [ data.azuread_client_config.azclconfig01.object_id ] 
 
 }  
