ccac9baa221017b662d5c212fec6f8fe376e8774
rahulnani2 <rahulnani2@gmail.com>
2025-12-13 14:13:17 -0500

    Removed the service prinicpal module from Infra scripts and added managed-identity for AKS

--- Infra/Terraform/DEV/main.tf
+++ Infra/Terraform/DEV/main.tf
@@ -35,29 +35,22 @@ output "network_details" {
     value = module.networkModule.subnet_id
 }
 
+/** -- Removed Service Prinicpal 
 module "ServicePrinicpal01" {
    source = "../modules/ServicePrincipal"
     service_principal_name = "dev-petclinic-sp01"
-}
+}  **/
 
 module "petclinicacr" {
-  depends_on = [module.ServicePrinicpal01]
    source = "../modules/acr"
    acr_name = "petclinicimages"
    resource_group_name = azurerm_resource_group.pgrg01.name
    location = azurerm_resource_group.pgrg01.location
 } 
 
-resource "azurerm_role_assignment" "azacrpullrole" {
-  depends_on = [module.ServicePrinicpal01, module.petclinicacr]
-  scope = module.petclinicacr.acr_id
-  role_definition_name = "AcrPull"
-  principal_id = module.ServicePrinicpal01.service_principal_objectid
-}
 
 module "AKSCluster01" {
-   depends_on = [ module.networkModule, module.petclinicacr, module.ServicePrinicpal01 ]
-   
+   depends_on = [ module.networkModule, module.petclinicacr ]
    source = "../modules/aks"
    resource_group_name = azurerm_resource_group.pgrg01.name
    location = azurerm_resource_group.pgrg01.location
@@ -65,9 +58,9 @@ module "AKSCluster01" {
    kubernetes_version = "1.33.5"
    aks_subnet_id = module.networkModule.subnet_id[var.aksnodepool_subnet]
    tags = local.requiredTags
-   service_principle_name = module.ServicePrinicpal01.service_principle_name
-   client_id = module.ServicePrinicpal01.client_id
-   client_secret = module.ServicePrinicpal01.client_secret
+  # service_principle_name = module.ServicePrinicpal01.service_principle_name
+  # client_id = module.ServicePrinicpal01.client_id
+  # client_secret = module.ServicePrinicpal01.client_secret
 } 
 
 resource "local_file" "kubeconfig" {
@@ -75,5 +68,10 @@ resource "local_file" "kubeconfig" {
   content = module.AKSCluster01.config
 }
 
-
+resource "azurerm_role_assignment" "azacrpullrole" {
+  principal_id = module.AKSCluster01.aks_pricinipal_id
+  scope = module.petclinicacr.acr_id
+  role_definition_name = "AcrPull"
+  skip_service_principal_aad_check = true
+} 
 
