60f3a8ccaef24e4de687c18ab4d0e0c8ac59c29a
rahul_dondeti <rahulnani2@gmail.com>
2025-12-12 21:01:49 -0500

    Refactor ServicePrincipal module by removing comments
    Removed unused resources and dependencies for clarity.

--- Infra/Terraform/modules/ServicePrincipal/main.tf
+++ Infra/Terraform/modules/ServicePrincipal/main.tf
@@ -1,29 +1,24 @@
-data "azuread_client_config" "azclconfig01" {
-}
+data "azuread_client_config" "azclconfig01" {}
 
 resource "azuread_application" "azapp01" {
    display_name = var.service_principal_name
    owners = [ data.azuread_client_config.azclconfig01.object_id]
 }
 
-resource "time_sleep" "wait_for_application" { 
-  depends_on = [azuread_application.azapp01]
-  create_duration = "60s"
-}
+#resource "time_sleep" "wait_for_application" { 
+#  depends_on = [azuread_application.azapp01]
+#  create_duration = "60s"
+#}
 
 resource "azuread_service_principal" "azadsp01" {
-   depends_on = [time_sleep.wait_for_application, azuread_application.azapp01]
+ #  depends_on = [time_sleep.wait_for_application, azuread_application.azapp01]
    app_role_assignment_required = true
    client_id = azuread_application.azapp01.client_id
    owners = [ data.azuread_client_config.azclconfig01.object_id ]
+ lifecycle { 
+   create_before_destroy = true 
+   }
 }  
-
-# resource "azuread_application_owner" "ad_owner" {
-#  depends_on = [azuread_application.azapp01, azuread_service_principal.azadsp01]
-#  application_object_id = azuread_application.azapp01.object_id
-#  owner_object_id = data.azuread_client_config.azclconfig01.object_id
- 
-# }
 
 resource "azuread_service_principal_password" "azadsppasswd01" {
   depends_on=[azuread_application.azapp01, azuread_service_principal.azadsp01]
